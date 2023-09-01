23d071cee559f068f3431f7eb4e33ca9c1229e90
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-09-02 00:20:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,25 @@
 
-// Function to load content based on menu selection
+// Function to load content based on the menu selection
 function loadContent(section) {
-  // Hide all content sections
-  const sections = document.querySelectorAll('.content-section');
-  sections.forEach((element) => {
+  // Hide all existing content sections
+  const contentSections = document.querySelectorAll('.content-section');
+  contentSections.forEach((element) => {
     element.style.display = 'none';
   });
 
   // Show the selected content section
+  const selectedSection = document.getElementById(section);
+  if (selectedSection) {
+    selectedSection.style.display = 'block';
+  }
+
+  // Place random GIFs
   placeRandomGifs();
-  document.getElementById(section).style.display = 'block';
 }
-
-// Function to show Blue Screen of Death (BSOD) as a pop-up
-function showBSOD() {
-  alert('Blue Screen of Death! Just kidding.');
-}
-
-// Function to make the website "freeze"
-function freezeWebsite() {
-  // Code to make the website unresponsive for a few seconds can be added here
-}
-
-// Loading Bar
-window.onload = function() {
-  placeRandomGifs();
-  // Code to simulate a loading bar can be added here
-};
 
 // Function to randomly place GIFs throughout the website
 function placeRandomGifs() {
-  // List of available GIFs
   const availableGifs = ['Rover.gif', 'UC.gif', 'UC2.gif', 'cats.gif', 'giphy.gif', 'loading.gif', 'popo.gif', 'rose.gif', 'w.gif'];
-
-  // Get the content area where GIFs will be placed
   const contentArea = document.querySelector('.content');
 
   // Clear any existing random GIFs
@@ -44,50 +30,80 @@ function placeRandomGifs() {
 
   // Place 5 random GIFs
   for (let i = 0; i < 5; i++) {
-    // Create a new image element for the GIF
     const gif = document.createElement('img');
-
-    // Assign a random GIF from the list
     const randomGifIndex = Math.floor(Math.random() * availableGifs.length);
     gif.src = availableGifs[randomGifIndex];
-
-    // Apply random styles
     const randomSize = Math.floor(Math.random() * 100) + 50;
     const randomSkew = Math.floor(Math.random() * 20) - 10;
     gif.style.width = `${randomSize}px`;
     gif.style.transform = `skew(${randomSkew}deg)`;
-
-    // Add class for identification
     gif.classList.add('random-gif');
-
-    // Place the GIF at a random position within the content area
     const randomTop = Math.floor(Math.random() * (contentArea.offsetHeight - 100));
     const randomLeft = Math.floor(Math.random() * (contentArea.offsetWidth - 100));
     gif.style.position = 'absolute';
     gif.style.top = `${randomTop}px`;
     gif.style.left = `${randomLeft}px`;
-
-    // Append the GIF to the content area
     contentArea.appendChild(gif);
   }
 }
 
-// Function to display BSOD.mp4 video in the "About" section
-function displayVideo() {
-  // Get the "About" content section
-  const aboutSection = document.getElementById('about');
+// Attach function to window load event
+window.onload = function() {
+  // Show initial content
+  loadContent('home');
+};
 
-  // Clear existing content
-  aboutSection.innerHTML = '';
+// Function to make windows draggable
+function makeDraggable(element) {
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  element.onmousedown = dragMouseDown;
 
-  // Create a new video element
-  const video = document.createElement('video');
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
 
-  // Set video attributes
-  video.src = 'BSOD.mp4';
-  video.controls = true;
-  video.autoplay = true;
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    element.style.top = (element.offsetTop - pos2) + "px";
+    element.style.left = (element.offsetLeft - pos1) + "px";
+  }
 
-  // Append the video to the "About" section
-  aboutSection.appendChild(video);
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
+
+// Function to create mouse trails
+function createMouseTrail() {
+  const trail = document.createElement('div');
+  trail.className = 'mouse-trail';
+  document.body.appendChild(trail);
+  let x = 0, y = 0;
+  const onMouseMove = (e) => {
+    x = e.pageX;
+    y = e.pageY;
+    trail.style.left = (x - 5) + 'px';
+    trail.style.top = (y - 5) + 'px';
+  };
+  document.addEventListener('mousemove', onMouseMove);
+}
+
+// Make content sections draggable
+const contentSections = document.querySelectorAll('.content-section');
+contentSections.forEach((element) => {
+  makeDraggable(element);
+});
+
+// Create mouse trail
+createMouseTrail();
