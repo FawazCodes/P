5daafd37f048cfbf1b9c9e591906b5804f729ce0
FawazCodes <108174119+FawazCodes@users.noreply.github.com>
2023-09-01 23:49:56 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ function freezeWebsite() {
 window.onload = function() {
   // Code to simulate a loading bar can be added here
 };
+
+
+
+// Function to randomly place GIFs throughout the website
+function placeRandomGifs() {
+  // List of GIFs to be randomly placed
+  const gifs = ['Rover.gif', 'w.gif', 'giphy.gif', 'cats.gif', 'popo.gif', 'UC.gif', 'UC2.gif', 'rose.gif', 'loading.gif'];
+
+  // Clear existing GIFs (except the visitor number GIF)
+  const existingGifs = document.querySelectorAll('.random-gif');
+  existingGifs.forEach((element) => {
+    element.remove();
+  });
+
+  // Get all content sections
+  const contentSections = document.querySelectorAll('.content-section');
+
+  // Loop through each GIF and place it randomly
+  gifs.forEach((gif) => {
+    // Choose a random content section
+    const randomSection = contentSections[Math.floor(Math.random() * contentSections.length)];
+
+    // Create a new image element for the GIF
+    const img = document.createElement('img');
+    img.src = gif;
+    img.className = 'random-gif';
+
+    // Apply random styles (size, skew)
+    const randomSize = Math.floor(Math.random() * 200) + 50;
+    const randomSkewX = Math.floor(Math.random() * 20) - 10;
+    const randomSkewY = Math.floor(Math.random() * 20) - 10;
+    img.style.width = randomSize + 'px';
+    img.style.height = randomSize + 'px';
+    img.style.transform = `skew(${randomSkewX}deg, ${randomSkewY}deg)`;
+
+    // Append the GIF to the random content section
+    randomSection.appendChild(img);
+  });
+}
